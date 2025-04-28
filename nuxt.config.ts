d30aa9a8bfa2e0ac9634a0ce23006d02690b7e1f
omar-labana <olabana@effvision.com>
2025-04-28 13:08:47 +0300

    feat: update Nuxt configuration and add additional dependencies for i18n, Swiper, and UI components

--- nuxt.config.ts
+++ nuxt.config.ts
@@ -2,5 +2,12 @@
 export default defineNuxtConfig({
   compatibilityDate: '2024-11-01',
   devtools: { enabled: true },
-  modules: ['@nuxtjs/tailwindcss']
-})
+  modules: [
+    '@nuxtjs/tailwindcss',
+    '@nuxtjs/i18n',
+    'nuxt-swiper',
+    '@nuxt/fonts',
+    '@nuxt/icon',
+    'reka-ui/nuxt',
+  ],
+});
